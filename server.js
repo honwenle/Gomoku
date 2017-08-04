9617b4291b0abc094d9f0a061e9e4da9e6d69e82
洪文乐 <noclip@foxmail.com>
2017-08-04 13:29:31 +0800

    数据移至服务端 todo: 绘制过去 todo: 高亮刚下的

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var joinlist = {
-    black: false,
-    white: false
-};
+var player = {};
+var black = [], white = [];
+var isBlack = true, isOver = false;
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname+'/index.html');
@@ -17,9 +16,69 @@ io.on('connection', function(socket){
 		io.emit('submit',msg)
 	});
     socket.on('move', function (xy) {
+        if (isOver) return false;
+        if(xy.p != (isBlack ? player.black : player.white)) {
+            return false
+        }
+        var num = getXY(xy.x, xy.y);
+        if (black.indexOf(num) > -1 || white.indexOf(num) > -1) {
+            return false
+        }
+        if (isBlack) {
+            black.push(num)
+        } else {
+            white.push(num)
+        }
+        xy.isBlack = isBlack;
         io.emit('move', xy);
+        checkLine(xy.x, xy.y);
+        isBlack = !isBlack;
+    });
+    socket.on('join', function (name) {
+        var fang = '旁观者';
+        if (!player.black || player.black==name) {
+            player.black = name;
+            fang = '黑方'
+        } else if (!player.white || player.white==name) {
+            player.white= name;
+            fang = '白方'
+        }
+        io.emit('join', {
+            name: name,
+            fang: fang
+        })
     });
 });
+function checkLine(x, y) {
+    var i = 1, s = 0;
+    var arr = isBlack ? black : white;
+    var xo = [[1,0], [1,1], [0,1], [1,-1]];
+    for (var o = 0; o < 4; o++) {
+        for (var r = 1,s = 0; r >= -1; r -= 2, i = 1) {
+            while (arr.indexOf(getXY(x + xo[o][0]*r*i*50, y + xo[o][1]*r*i*50)) > -1) {
+                i++;
+                s++;
+                if (s >= 4) {
+                    isOver = true;
+                    io.emit('win', ~~!isBlack);
+                    reset();
+                    return false;
+                }
+            }
+        }
+    }
+}
+function getXY(x, y) {
+    if (x < 0 || y < 0) {
+        return -1;
+    }
+    return (y-y%50)*50 + x-x%50;
+}
+function reset() {
+    player = {};
+    black = [], white = [];
+    isBlack = true, isOver = false;
+}
 
 http.listen(3000, function(){
 	console.log('listening on port 3000')
